0c879979641af92e9fdf493c28b63edfbfe3ead1
root <root@DESKTOP-K6987B3.localdomain>
2022-06-25 21:32:54 -0700

    Added new token system.

--- quotebot.py
+++ quotebot.py
@@ -1,6 +1,11 @@
 import hikari
 
-bot = hikari.GatewayBot(token='OTkwNDM0MDI1MDExMjkwMTMy.GnTV_n.L_nnVBVVH0u3k7aiDD3huup9RHwkcC9obBV5oQ')
+text_file = open("./token.txt")
+new_token = text_file.read()
+text_file.close()
+print(new_token)
+
+bot = hikari.GatewayBot(token=new_token)
 
 @bot.listen(hikari.GuildMessageCreateEvent)
 async def print_message(event):
