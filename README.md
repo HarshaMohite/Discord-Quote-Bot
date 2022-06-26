816e34f37f9c17f4f13a8ed4239e0784e868963f
root <root@DESKTOP-K6987B3.localdomain>
2022-06-26 13:43:27 -0700

    Removed test commands. Cleaned up quote. Added token and quote database notes to readme.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Discord-Quote-Bot
 Discord bot for recording out of context quotes in your server.
+
+Place bot token in a new file titled 'token.txt'.
+
+Create a txt file titled 'quotedata.txt' for storing quotes.
--- quotebot.py
+++ quotebot.py
@@ -2,51 +2,13 @@ import random
 import hikari
 import lightbulb
 
+# load token
 text_file = open("./token.txt")
 new_token = text_file.read()
 text_file.close()
 print(new_token)
 
-bot = lightbulb.BotApp(token=new_token, default_enabled_guilds=())
-
-@bot.listen(hikari.GuildMessageCreateEvent)
-async def print_message(event):
-    print(event.content)
-
-@bot.listen(hikari.StartedEvent)
-async def bot_started(event):
-    print('Bot has started!')
-    
-@bot.command
-@lightbulb.command('zznewquote', 'Stores the given quote.')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def ping(contextObject):
-    await contextObject.respond('Pong!')
-    
-@bot.command
-@lightbulb.command('group', 'This is a group')
-@lightbulb.implements(lightbulb.SlashCommandGroup)
-async def my_group(contextObject):
-    pass
-
-@my_group.child
-@lightbulb.command('subcommand', 'This is a subcommand')
-@lightbulb.implements(lightbulb.SlashSubCommand)
-async def subcommand(contextObject):
-    await contextObject.respond('I am a subcommand!')
-    
-@bot.command
-@lightbulb.option('num1', 'The first number', type=int)
-@lightbulb.option('num2', 'The second number', type=int)
-@lightbulb.command('add', 'Add two numbers together')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def add(contextObject):
-    await contextObject.respond(contextObject.options.num1 + contextObject.options.num2)
-    
-    
-    
-    
-# actual stuff starts here
+bot = lightbulb.BotApp(token=new_token)
 
 # quote data file
 databaseFile = "./quotedata.txt"
@@ -84,7 +46,7 @@ def getQuote():
 # Option - Amount: Returns this many random quotes.
 @bot.command
 @lightbulb.option('amount', 'Number of quotes to spit out.', type=int)
-@lightbulb.command('quote', 'Random quotes.')
+@lightbulb.command('quotes', 'Print multiple random quotes.')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def quote(context):
     quotestring = ""
@@ -93,7 +55,7 @@ async def quote(context):
     await context.respond(quotestring)
     
 @bot.command
-@lightbulb.command('quote')
+@lightbulb.command('quote', 'Print a random quote.')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def quote(context):
     await context.respond(getQuote() + "\n")
