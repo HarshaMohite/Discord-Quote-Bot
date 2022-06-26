4fa9492e41e3a9b4fc85d4a717ac125cf0e872e3
root <root@DESKTOP-K6987B3.localdomain>
2022-06-26 13:30:34 -0700

    Added add and retrieve quote commands.

--- quotebot.py
+++ quotebot.py
@@ -1,3 +1,4 @@
+import random
 import hikari
 import lightbulb
 
@@ -17,7 +18,7 @@ async def bot_started(event):
     print('Bot has started!')
     
 @bot.command
-@lightbulb.command('newquote', 'Stores the given quote.')
+@lightbulb.command('zznewquote', 'Stores the given quote.')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def ping(contextObject):
     await contextObject.respond('Pong!')
@@ -40,7 +41,81 @@ async def subcommand(contextObject):
 @lightbulb.command('add', 'Add two numbers together')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def add(contextObject):
-    await contextObject.respond(ctx.options.num1 + ctx.options.num2)
+    await contextObject.respond(contextObject.options.num1 + contextObject.options.num2)
+    
+    
+    
+    
+# actual stuff starts here
+
+# quote data file
+databaseFile = "./quotedata.txt"
+
+# stores all quotes during runtime
+# refreshed on every readToArray() call
+database = open(databaseFile)
+quotearray = database.readlines()
+database.close
+
+# read all quote data lines to quotearray
+# does a full refresh of the quotearray
+def readToArray():
+    database = open(databaseFile)
+    quotearray = database.readlines()
+    database.close
+    
+#initial read
+readToArray()
+
+# writes to file and appends to end of quotearray
+# does not do a full refresh of quotearray
+def writeNewQuote(quotetext):
+    database = open(databaseFile, "a") # open for append
+    database.write(quotetext + "\n")
+    quotearray.append(quotetext)
+    database.close
+
+#grab a random quote
+def getQuote():
+    quoteArrayLen = len(quotearray)
+    return quotearray[random.randrange(0, (quoteArrayLen), 1)]
+
+# Command: Get quote.
+# Option - Amount: Returns this many random quotes.
+@bot.command
+@lightbulb.option('amount', 'Number of quotes to spit out.', type=int)
+@lightbulb.command('quote', 'Random quotes.')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def quote(context):
+    quotestring = ""
+    for x in range(context.options.amount):
+        quotestring += getQuote() + "\n"    
+    await context.respond(quotestring)
+    
+@bot.command
+@lightbulb.command('quote')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def quote(context):
+    await context.respond(getQuote() + "\n")
+        
+# Command: Add new quote to database.
+@bot.command
+@lightbulb.option('quote', 'Quote to be added.', type=str)
+@lightbulb.command('newquote', 'Adds new quote.')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def newquote(context):
+    writeNewQuote(context.options.quote)
+    await context.respond('Quote added.')
+    
+@bot.command
+@lightbulb.option('quote', 'Quote to be added.', type=str)
+@lightbulb.command('addquote', 'Adds new quote.')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def addquote(context):
+    writeNewQuote(context.options.quote)
+    await context.respond('Quote added.')
     
 
+
+# finally, run the bot
 bot.run()
