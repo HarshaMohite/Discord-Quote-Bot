6ec915c40ad3279956436fdf8d73432c94628384
root <root@DESKTOP-K6987B3.localdomain>
2022-06-26 21:35:39 -0700

    Refactored options into Slash and Prefix command versions. Formatted code. Added checks for quotes with bot commands or duplicate quotes.

--- quotebot.py
+++ quotebot.py
@@ -1,3 +1,5 @@
+from contextlib import nullcontext
+import math
 import random
 import hikari
 import lightbulb
@@ -8,7 +10,7 @@ new_token = text_file.read()
 text_file.close()
 print(new_token)
 
-bot = lightbulb.BotApp(token=new_token)
+bot = lightbulb.BotApp(token=new_token, prefix=".")
 
 # quote data file
 databaseFile = "./quotedata.txt"
@@ -31,52 +33,96 @@ readToArray()
 
 # writes to file and appends to end of quotearray
 # does not do a full refresh of quotearray
-def writeNewQuote(quotetext):
+def writeNewQuote(quotetext):    
+    if ".quote" in quotetext.lower():
+        return False
+    if ".addquote" in quotetext.lower():
+        return False
+    if ".newquote" in quotetext.lower():
+        return False
+    if "!quote" in quotetext.lower():
+        return False
+    if "!addquote" in quotetext.lower():
+        return False
+    if "!newquote" in quotetext.lower():
+        return False
+    quotetext_appended = quotetext + "\n" # Need to check for duplicates with newline char at end
+    if quotetext_appended in quotearray:
+        return False
     database = open(databaseFile, "a") # open for append
-    database.write(quotetext + "\n")
-    quotearray.append(quotetext)
+    database.write(quotetext_appended)
+    quotearray.append(quotetext_appended)
     database.close
+    return True
 
 #grab a random quote
 def getQuote():
     quoteArrayLen = len(quotearray)
     return quotearray[random.randrange(0, (quoteArrayLen), 1)]
 
-# Command: Get quote.
-# Option - Amount: Returns this many random quotes.
-@bot.command
-@lightbulb.option('amount', 'Number of quotes to spit out.', type=int)
-@lightbulb.command('quotes', 'Print multiple random quotes.')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def quote(context):
+# Get multiple quotes.
+# Option - Amount: Print this many random quotes. Max 10.
+def getQuotes(amount):
     quotestring = ""
-    for x in range(context.options.amount):
-        quotestring += getQuote() + "\n"    
-    await context.respond(quotestring)
+    for x in range(min(10, max(1, amount))):
+        quotestring += getQuote()
+    return quotestring
     
+
+# COMMAND #
+# quote
 @bot.command
+@lightbulb.option('amount', 'Number of quotes to print.', type=int, required=False)
 @lightbulb.command('quote', 'Print a random quote.')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def quote(context):
-    await context.respond(getQuote() + "\n")
+    if context.options.amount == None:
+        await context.respond(getQuote())
+    else:
+        await context.respond(getQuotes(context.options.amount))
         
-# Command: Add new quote to database.
+@bot.command
+@lightbulb.option('amount', 'Number of quotes to print.', type=int, required=False)
+@lightbulb.command('quote', 'Print a random quote.')
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def quote(context):
+    if context.options.amount == None:
+        await bot.rest.create_message(context.get_channel().id, getQuote())
+    else:
+        await bot.rest.create_message(context.get_channel().id, getQuotes(context.options.amount))
+        
+        
+# COMMAND #
+# newquote / addquote
 @bot.command
 @lightbulb.option('quote', 'Quote to be added.', type=str)
-@lightbulb.command('newquote', 'Adds new quote.')
+@lightbulb.command('newquote', 'Adds new quote.', aliases=["addquote"])
 @lightbulb.implements(lightbulb.SlashCommand)
 async def newquote(context):
-    writeNewQuote(context.options.quote)
-    await context.respond('Quote added.')
+    if writeNewQuote(context.options.quote):
+        await context.respond("Quote added: " + context.options.quote)
+    else:
+        await context.respond("Quote invalid.")
     
 @bot.command
 @lightbulb.option('quote', 'Quote to be added.', type=str)
-@lightbulb.command('addquote', 'Adds new quote.')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def addquote(context):
-    writeNewQuote(context.options.quote)
-    await context.respond('Quote added.')
-    
+@lightbulb.command('newquote', 'Adds new quote.', aliases=["addquote"])
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def newquote(context):
+    if writeNewQuote(context.options.quote):
+        await bot.rest.create_message(context.get_channel().id, "Quote added: " + context.options.quote)
+    else:
+        await bot.rest.create_message(context.get_channel().id, "Quote invalid. Already in database or includes a bot command.")
+
+
+# if valid event, give a reply to "quote"
+# @bot.listen(hikari.GuildMessageCreateEvent)
+# async def print_message(event):
+#     if event != None:
+#         if "quote" in event.message.content.lower():
+#             #await bot.rest.create_message(event.channel_id, "Found the magic words!")
+#             pass
+
 
 
 # finally, run the bot
